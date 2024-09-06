f08b0252d1c3863906ac871752420732231e0065
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2024-09-07 04:29:04 +0600

    js code added

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,23 @@
+// function play() {
+//     //hide the home screen
+//     const homeSection = document.getElementById("home-screen");
+//     homeSection.classList.add("hidden");
+//     //show the playground
+//
+//     const playGroupSection = document.getElementById("play-ground");
+//     playGroupSection.classList.remove("hidden");
+// }
+
 function play() {
-    console.log("Don't watch keyboard  while playing...")
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
+
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+    console.log(alphabet);
+    const currentAlphabet = document.getElementById("current-alphabet");
+    currentAlphabet.innerText = alphabet;
+    setBackgroundColorById(alphabet);
 }
