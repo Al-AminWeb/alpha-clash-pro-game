7fee28bc16f231fc0cc415fc5fd5f0493e3eefaf
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2024-09-13 23:15:05 +0600

    score and life are functional

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,35 @@ function play() {
     continueGame()
 }
 
+function handleKeyboardButtonPress(event) {
+    const playerPressed = event.key;
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    if (playerPressed === currentAlphabet) {
+        // update score
+
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText
+        const currentScore = parseInt(currentScoreText);
+        const newScore = currentScore +1
+
+        currentScoreElement.innerText = newScore;
+        continueGame();
+        removeBackgroundColorById(expectedAlphabet)
+    } else {
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText
+        const currentLife = parseInt(currentLifeText);
+        const newLife = currentLife -1;
+        currentLifeElement.innerText = newLife;
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
 function continueGame() {
     const alphabet = getARandomAlphabet();
     console.log(alphabet);
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -19,3 +19,8 @@ function setBackgroundColorById(elementId) {
     const currentAlphabetElement = document.getElementById(elementId);
     currentAlphabetElement.classList.add('bg-orange-400');
 }
+
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
